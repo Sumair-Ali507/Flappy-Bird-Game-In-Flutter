4885302744e7483e8327ee6b712629ac1ebd24f0
Sumair Ali <sumairaliphulpoto1232@gmail.com>
2024-10-25 19:35:02 +0500

    Flappy Bird Game

---
+++ flappy_bird_game/lib/Screens/game_over_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flappy_bird_game/Game/flappy_bird_game.dart';
+import 'package:flutter/material.dart';
+
+class GameOverScreen extends StatelessWidget {
+  static const String id = 'gameOver';
+  final FlappyBirdGame game;
+
+  const GameOverScreen({super.key, required this.game});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- flappy_bird_game/lib/main.dart
+++ flappy_bird_game/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flappy_bird_game/Game/flappy_bird_game.dart';
 import 'package:flame/game.dart';
+import 'package:flappy_bird_game/Screens/main_menu_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   final game = FlappyBirdGame();
   runApp(
-    GameWidget(game: game), // Ensure that GameWidget is used properly
+    GameWidget(game: game,
+      initialActiveOverlays: const [MainMenuScreen.id],
+      overlayBuilderMap: {
+      'mainMenu': (context, _) => MainMenuScreen(game: game),
+      },
+    ), // Ensure that GameWidget is used properly
   );
 }
 
